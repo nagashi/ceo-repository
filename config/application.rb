7a8bce352a150b432b8431e6891ff94161dd62a9
nagashi <ceoriley@gmail.com>
2019-03-08 05:26:31 +0000

    Week 2, Lesson 10

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require_relative 'boot'
+# (my comment) require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# (my comment) Bundler.require(:default, Rails.env)
 
 module Splurty
   class Application < Rails::Application
@@ -15,5 +17,12 @@ module Splurty
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+     # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+
   end
 end
